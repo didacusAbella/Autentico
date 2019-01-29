c4d90a87d9277dde30ee3b72916a160f0631c471
Giammaria Giordano <giammagiorda@gmail.com>
2019-01-29 14:32:01 +0100

    fix back pressed

--- client/src/pages/product-found/product-found.ts
+++ client/src/pages/product-found/product-found.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
 
-/**
- * Generated class for the ProductFoundPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-product-found',
