34cf9298380a9f7d9fd410b14e48587e81e90f2b
didacusabella <diequito93@gmail.com>
2019-02-05 12:07:19 +0100

    finish edit components

--- dashboard/src/app/components/colors/color.ts
+++ dashboard/src/app/components/colors/color.ts
@@ -1,4 +1,9 @@
-export interface Color {
-  id: number;
-  hex: string;
+export class Color {
+  public id: number;
+  public hex: string;
+
+  constructor(params) {
+    this.id = params.id;
+    this.hex = params.hex;
+  }
 }
